88c03e833260930b8c870c263b7673a1eab1a343
Yelzhan <tuigynbekov@gmail.com>
2021-02-15 14:57:55 +0600

    Initial Commit

---
+++ WorkWithMqtt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkWithMqtt
+//
+//  Created by greetgo on 2/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
