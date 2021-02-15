dea07f4fb96b515b08ecceddc34cdaaed4a76be7
Yelzhan <tuigynbekov@gmail.com>
2021-02-15 15:32:29 +0600

    Add mqtt params

--- WorkWithMqtt/Modules/Main/MainViewController.swift
+++ WorkWithMqtt/Modules/Main/MainViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    // MARK:- View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = UIColor.orange.withAlphaComponent(0.3)
+        setupViews()
     }
 
+    // MARK:- Autolayout
+    func setupViews() {
+        view.backgroundColor = UIColor.orange.withAlphaComponent(0.3)
+    }
 }
