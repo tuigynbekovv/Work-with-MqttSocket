e01ff219b042ef7d6fc7711cd0635270ccf72bc3
Yelzhan <tuigynbekov@gmail.com>
2021-02-15 15:09:00 +0600

    Build app

---
+++ WorkWithMqtt/Modules/Main/MainViewController.swift
@@ -0,0 +1,18 @@
+/*
+ *  MainViewController.swift
+ *  WorkWithMqtt
+ *
+ *  Created by tuigynbekov on 2/15/21.
+ */
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.orange.withAlphaComponent(0.3)
+    }
+
+}
